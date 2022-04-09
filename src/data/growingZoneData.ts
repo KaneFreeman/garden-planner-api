b5dcb048db4a268194b7aff25530028b3710931a
Daniel Lautzenheiser <lautzd@gmail.com>
2022-04-09 15:21:57 -0400

    Linting

--- src/data/growingZoneData.ts
+++ src/data/growingZoneData.ts
@@ -3,7 +3,7 @@ const growingZoneData = {
   last_frost: 'May 01',
   first_frost: 'October 10',
   full_first_frost: '10/10/2022',
-  full_last_frost: '01/05/2022'
+  full_last_frost: '01/05/2022',
 };
 
 export default growingZoneData;
